ffc18d07a19a6f26917e4b9008ec8d0fdddf0679
Felipe Vergara <favazcl@gmail.com>
2022-01-14 20:07:00 -0300

    init storage api

--- packages/local_storage_articles_api/lib/src/local_storage_articles_api.dart
+++ packages/local_storage_articles_api/lib/src/local_storage_articles_api.dart
@@ -1,7 +1,30 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:articles_api/articles_api.dart';
+import 'package:rxdart/subjects.dart';
+
 /// {@template local_storage_articles_api}
 /// A Very Good Project created by Very Good CLI.
 /// {@endtemplate}
-class LocalStorageArticlesApi {
+class LocalStorageArticlesApi extends ArticlesApi {
   /// {@macro local_storage_articles_api}
   const LocalStorageArticlesApi();
+
+  final _articleStreamController = BehaviorSubject<List<Article>>.seeded(const []);
+
+  void _init() {
+    final articlesJson = _getValue(kTodosCollectionKey);
+    if (articlesJson != null) {
+      final todos = List<Map>.from(json.decode(articlesJson) as List)
+          .map((jsonMap) => Article.fromJson(Map<String, dynamic>.from(jsonMap)))
+          .toList();
+      _articleStreamController.add(todos);
+    } else {
+      _articleStreamController.add(const []);
+    }
+  }
+
+  @override
+  Stream<List<Article>> getTodos() => _articleStreamController.asBroadcastStream();
 }
