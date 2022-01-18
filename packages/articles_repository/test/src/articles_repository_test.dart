cd1faa6dbbbde2a5ca32fd918b15053c6a51198e
Felipe Vergara <favazcl@gmail.com>
2022-01-18 00:37:07 -0300

    c

--- packages/articles_repository/test/src/articles_repository_test.dart
+++ packages/articles_repository/test/src/articles_repository_test.dart
@@ -1,17 +1,36 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:articles_api/articles_api.dart';
 import 'package:articles_repository/articles_repository.dart';
+import 'package:articles_repository/utils/network_info.dart';
+import 'package:local_storage_articles_api/local_storage_articles_api.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 class MockTodosApi extends Mock implements ArticlesApi {}
 
 class FakeArticle extends Fake implements Article {}
+
 void main() {
-  // late ArticlesApi api;
-  group('ArticlesRepository', () {
-    test('can be instantiated', () {
-      // expect(ArticlesRepository(articlesApi: api), isNotNull);
-    });
-  });
+  late ArticlesApi articlesApi;
+  late LocalStorageArticlesApi localStorageArticlesApi;
+  late NetworkInfo networkInfo;
+  group(
+    'ArticlesRepository',
+    () {
+      /*
+      test(
+        'can be instantiated',
+        () {
+          expect(
+            ArticlesRepository(
+              articlesApi: articlesApi,
+              localStorageArticlesApi: localStorageArticlesApi,
+              networkInfo: networkInfo,
+            ),
+            isNotNull,
+          );
+        },
+      );*/
+    },
+  );
 }
--- packages/local_storage_articles_api/lib/src/local_storage_articles_api.dart
+++ packages/local_storage_articles_api/lib/src/local_storage_articles_api.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:articles_api/articles_api.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
-import 'package:rxdart/subjects.dart';
 
 /// {@template local_storage_articles_api}
 /// A Very Good Project created by Very Good CLI.
@@ -11,43 +10,19 @@ import 'package:rxdart/subjects.dart';
 class LocalStorageArticlesApi extends ArticlesApi {
   /// {@macro local_storage_articles_api}
   LocalStorageArticlesApi() {
-    // _init();
     _initHive();
   }
 
-  void _initHive() async {
-    final directory = await path_provider.getApplicationDocumentsDirectory();
-  Hive
-    ..init(directory.path)
-    ..registerAdapter(ArticleAdapter());
-  }
-
-  List<Article> _articles = <Article>[];
-  final _articleStreamController =
-      BehaviorSubject<List<Article>>.seeded(const []);
-
-  Future<void> _init() async {
+  /// Initialization of Hive
+  Future<void> _initHive() async {
     final directory = await path_provider.getApplicationDocumentsDirectory();
     Hive
       ..init(directory.path)
       ..registerAdapter(ArticleAdapter());
-
-    final box = await Hive.openBox<Article>('article');
-
-    if (box.isNotEmpty) {
-      _articles = box.values.toList();
-      _articleStreamController.add(_articles);
-    } else {
-      _articleStreamController.add(const []);
-    }
   }
 
-  /// Save articles in the local storage
+  /// Save articles in the Local Storage
   Future<void> saveArticles(List<Article> articles) async {
-    // final directory = await path_provider.getApplicationDocumentsDirectory();
-    // Hive
-    //   ..init(directory.path)
-    //   ..registerAdapter(ArticleAdapter());
     final box = await Hive.openBox<Article>('article');
 
     if (box.values.isEmpty) {
@@ -55,29 +30,14 @@ class LocalStorageArticlesApi extends ArticlesApi {
         await box.add(article);
       }
     }
-
-    print('done: ${articles}');
   }
 
-  // TODO: REPLACE THIS METHOD FOR OTHER, HAY QUE AGREGAR UNO DE BOX :D
   @override
   Future<List<Article>> getArticles({
     Map<String, dynamic>? queryParams,
   }) async {
-    try {
-      //    final directory = await path_provider.getApplicationDocumentsDirectory();
-      // Hive
-      //   ..init(directory.path)
-      //   ..registerAdapter(ArticleAdapter());
-      final box = await Hive.openBox<Article>('article');
-      print('box: ${box.values}');
-      _articles = box.values.toList();
-      print(_articles);
-      return _articles;
-    } catch (e) {
-      print(e);
-      return [];
-    }
+    final box = await Hive.openBox<Article>('article');
+
+    return box.values.toList();
   }
-  // _articles;
 }
